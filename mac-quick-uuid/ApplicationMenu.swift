5fe2cda4b6da69e2a4d5d14cdf0f14abd13312dd
Greg Ford <gregford@Gregs-MacBook-Pro.local>
2023-02-17 22:07:16 +0000

    get mvp working

---
+++ mac-quick-uuid/ApplicationMenu.swift
@@ -0,0 +1,26 @@
+//
+//  ApplicationMenu.swift
+//  mac-quick-uuid
+//
+//  Created by Greg Ford on 17/02/2023.
+//
+
+import Foundation
+import SwiftUI
+
+class ApplicationMenu: NSObject {
+    let menu = NSMenu()
+    
+    func createMenu() -> NSMenu {
+        let uuidView = UuidView()
+        let topView = NSHostingController(rootView: uuidView)
+        topView.view.frame.size = CGSize(width: 400, height: 35)
+        
+        let customMenuItem = NSMenuItem()
+        customMenuItem.view = topView.view
+        
+        menu.addItem(customMenuItem)
+        
+        return menu
+    }
+}
