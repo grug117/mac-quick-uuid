6c31243998c9d9e3ce5c27bd4e247991c751f1c9
Greg Ford <gregford@Gregs-MacBook-Pro.local>
2023-02-17 19:24:21 +0000

    Initial Commit

---
+++ mac-quick-uuid/mac_quick_uuidApp.swift
@@ -0,0 +1,17 @@
+//
+//  mac_quick_uuidApp.swift
+//  mac-quick-uuid
+//
+//  Created by Greg Ford on 17/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct mac_quick_uuidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
