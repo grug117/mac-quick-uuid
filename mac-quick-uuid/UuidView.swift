081b99e5f7eaa90acc8e8773641c490acb7ad775
grug117 <gregford117@live.com>
2023-02-18 13:27:22 +0000

    show poopover to confirm text was copied to clipboard

--- mac-quick-uuid/UuidView.swift
+++ mac-quick-uuid/UuidView.swift
@@ -6,7 +6,7 @@ private var pasteboard = NSPasteboard.general
 
 struct UuidView: View {
     @State private var uuid = UUID().uuidString
-    @AppStorage("idVersion") var idVersion = 4
+    @State private var showCopiedConfirm: Bool = false
     
     func uuidRefresh() -> Void {
         uuid = UUID().uuidString
@@ -15,14 +15,24 @@ struct UuidView: View {
     func uuidCopyToClipboard() -> Void {
         pasteboard.clearContents()
         pasteboard.setString(uuid, forType: .string)
+        showCopiedConfirm = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.7) {
+            showCopiedConfirm = false
+        }
     }
     
     var body: some View {
         HStack(alignment: .center) {
             Text(uuid)
+
             Button(action: uuidCopyToClipboard){
                 Image(systemName: "doc.on.doc")
             }
+            .popover(isPresented: $showCopiedConfirm, arrowEdge: .bottom) {
+                Text("Copied")
+                    .padding(.all, 10)
+            }
+
             Button(action: uuidRefresh){
                 Image(systemName: "arrow.clockwise")
             }
